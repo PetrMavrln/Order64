05ea804b0875bbf1b44c172c4f655a5ebae576ee
Petr Mavrin <petr.mavrin@gmail.com>
2023-06-18 15:43:47 +0400

    tweaks

--- order64/src/App.tsx
+++ order64/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import AppRouter from './components/AppRouter';
 import './scss/App.css';
 
