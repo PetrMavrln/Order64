3d57a84302509d5bb88ab9e382bdf6bca9555e57
Petr Mavrin <petr.mavrin@gmail.com>
2024-12-15 00:46:27 +0400

    fix(services): added examples imgs;

--- order64/src/pages/Services/Services.tsx
+++ order64/src/pages/Services/Services.tsx
@@ -1,4 +1,11 @@
 import { Helmet, HelmetProvider } from "react-helmet-async";
+import img1 from "../../assets/services/1.png";
+import img2 from "../../assets/services/2.png";
+import img3 from "../../assets/services/3.png";
+import img4 from "../../assets/services/4.png";
+import img5 from "../../assets/services/5.png";
+import img6 from "../../assets/services/6.png";
+import { Carousel } from "../../components/Carousel/Carousel";
 import { FooterComponent } from "../../components/FooterComponent";
 import { HeaderComponent } from "../../components/HeaderComponent";
 import { LeftPanel } from "../../components/LeftPanel/LeftPanelComponent";
@@ -8,6 +15,15 @@ import { ServiceLinkItem } from "./ServiceLinkItem/ServiceLinkItem";
 import styles from "./styles.module.scss";
 
 export const Services = () => {
+  const examplesImgs = [
+    { src: img1, alt: "Пример работы №1" },
+    { src: img2, alt: "Пример работы №2" },
+    { src: img3, alt: "Пример работы №3" },
+    { src: img4, alt: "Пример работы №4" },
+    { src: img5, alt: "Пример работы №5" },
+    { src: img6, alt: "Пример работы №6" },
+  ];
+
   const services = [
     { id: 0, title: "Скульптура" },
     { id: 1, title: "Бюст" },
@@ -42,6 +58,8 @@ export const Services = () => {
                   <ServiceLinkItem service={service} key={service.id} />
                 ))}
               </nav>
+              <ThematicBreak />
+              <Carousel title="Примеры работ" imgs={examplesImgs} />
               <ThematicBreak />
             </main>
             <RightPanel />
